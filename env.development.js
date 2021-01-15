7da93d563b5dfa78b93e3869f955cc1e42966038
yangyongji79 <yangyongji79@gmail.com>
2021-01-15 13:00:11 +0900

    「この商品の組み合わせでは決済できません」というエラーが対応改修

--- env.development.js
+++ env.development.js
@@ -3,6 +3,6 @@ module.exports = {
   ROBOTS: "noindex",
   BASE_URL: "https://dev-member-imabari.a.kuroco.app",
   PAYGENT_JS: "https://sandbox.paygent.co.jp/js/PaygentToken.js",
-  PAYGENT_MERCHANT_ID: "40989",
+  PAYGENT_MERCHANT_ID: "41062",
   PAYGENT_TOKEN: "test_msHzJWuyJwo326G2OTUhts2g",
 }
