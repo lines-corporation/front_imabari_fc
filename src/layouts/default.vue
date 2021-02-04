7d8007bfa696256f68fb9b286a1d6d3c96f50aff
yangyongji79 <yangyongji79@gmail.com>
2021-02-04 10:50:33 +0900

    TICKET_SYSTEM-380 【FC今治】お問い合わせボタンの修正対応

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -66,9 +66,12 @@
 
     <v-footer color="#171C61" padless app absolute inset>
       <v-row justify="center" no-gutters>
-        <v-btn color="white" text rounded class="my-2" to="/inquiry">
+        <v-btn color="white" text rounded class="my-2" :href="inquiry_url">
           お問い合わせ
         </v-btn>
+        <!-- <v-btn color="white" text rounded class="my-2" to="/inquiry">
+          お問い合わせ
+        </v-btn> -->
         <v-btn color="white" text rounded class="my-2" to="/faq">
           よくある質問
         </v-btn>
@@ -110,6 +113,7 @@
 export default {
   data() {
     return {
+      inquiry_url: "https://docs.google.com/forms/d/e/1FAIpQLSdYPh3YEm5fzrCxBVnEd64xvSVpHBwpaeKan0ODSa8thr6Gtw/viewform",
       clipped: false,
       drawer: false,
       fixed: false,
