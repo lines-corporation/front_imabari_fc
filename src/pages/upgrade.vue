e51a44a006d650ee7e3ddfde70962145514bf4cb
yangyongji79 <yangyongji79@gmail.com>
2021-02-19 17:26:30 +0900

    yangyongji79

--- src/pages/upgrade.vue
+++ src/pages/upgrade.vue
@@ -181,7 +181,7 @@ export default {
     this.cardNumberTemp = this.otherCardMask
   },
   methods: {
-    purchase() {
+    async purchase() {
       this.loading = true
       if (this.$refs.form3.validate()) {
         let self = this
@@ -263,12 +263,13 @@ export default {
             .then(() => {
               self.$store.dispatch(
                 "snackbar/setMessage",
-                self.success_message +
-                  "メールをご確認の上、決済手続きをお願いいたします。"
+                self.success_message
               )
               self.$store.dispatch("snackbar/snackOn")
-              self.$router.push("/")
-              self.loading = false
+              self.$auth.fetchUser().then(() => {
+                self.$router.push("/")
+                self.loading = false
+              })
             })
             .catch(function (error) {
               self.$store.dispatch(
