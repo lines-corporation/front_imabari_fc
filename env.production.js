477c95250e397d0d4bc6b929ba648879bf644cd1
yangyongji79 <yangyongji79@gmail.com>
2021-02-25 19:14:37 +0900

    ソースを改修

--- env.production.js
+++ env.production.js
@@ -5,7 +5,7 @@ module.exports = {
   PAYGENT_JS: "https://token.paygent.co.jp/js/PaygentToken.js",
   PAYGENT_MERCHANT_ID: "49117",
   PAYGENT_TOKEN: "live_zcVWRwRdcjZXqdDHt2yfOEl2",
-  APPAREL_CATEGORY_IDS: [33,38],
+  APPAREL_CATEGORY_IDS: [33, 38, 54, 49, 59],
   SEASON_PASS_CATEGORY_ID: 34,
   FIREBASE_API_KEY: "AIzaSyDYSxu-2l4jI9Odqp1TC-fVUqpRGTABv8w",
   FIREBASE_AUTH_ADMIN: "kuroco-member-imabari.firebaseapp.com",
