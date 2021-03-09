54d13ef5b8c761c331044712c88c9631e2850795
yangyongji79 <yangyongji79@gmail.com>
2021-03-09 10:37:15 +0900

    ソースを改修

---
+++ src/pages/buttonBlock.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-share-buttons :title="title" />
+</template>
+
+<script>
+import shareButtons from '~/components/atoms/shareButtons'
+
+export default {
+  components: {
+    shareButtons
+  },
+  data: function() {
+    return {
+      title: 'sns-widget'
+    }
+  }
+}
+</script>
--- src/pages/ticket/_id/index.vue
+++ src/pages/ticket/_id/index.vue
@@ -122,7 +122,7 @@
                                   <td class="text-left">
                                     <br />
                                     <span v-text="prodcut_nm(order_detail.product_id)" /><br />
-                                    {{ order_detail.price }}円<br />
+                                    {{ order_detail.price*parseInt(order_detail.quantity) }}円<br />
                                     {{ order_detail.quantity }}枚<br />
                                     <div v-if="prodcut_nm(order_detail.product_id).search('自由席') == -1 && order.note != 0 && order.note != null ">
                                        ゾーン {{ order.note.split('-')[0] }} / 座席 {{ order.note.split('-')[1].substring(0,3) }} <br/>
