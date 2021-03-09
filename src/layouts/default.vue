a4ff0664cb2fa75e55899cd35c18fe732f4563e5
yangyongji79 <yangyongji79@gmail.com>
2021-03-09 17:00:56 +0900

    ソースを改修

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -71,23 +71,12 @@
         <v-btn color="white" text rounded class="my-2" :href="inquiry_url" target="_blank">
           お問い合わせ
         </v-btn>
-        <!-- <v-btn color="white" text rounded class="my-2" to="/inquiry">
-          お問い合わせ
-        </v-btn> -->
         <v-btn color="white" text rounded class="my-2" to="/faq">
           よくある質問
         </v-btn>
         <v-btn color="white" text rounded class="my-2" to="/tokutei">
           特定商取引法に基づく表示
         </v-btn>
-        <!-- <v-col class="#171C61 text-center white--text" cols="12">
-
-          <img
-            src="@/assets/images/temp_icon.png"
-            style="height: 100px; padding: 5px;"
-          />
-
-        </v-col> -->
         <v-col class="#171C61 text-center white--text" cols="12">
           <span>Copyright FC Imabari. All rights reserved.</span>
         </v-col>
