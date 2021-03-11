cfbbe30a9ee97201e20bd87cb409db7a09a8a6c5
yangyongji79 <yangyongji79@gmail.com>
2021-03-11 12:59:23 +0900

    ソースを改修

--- env.development.js
+++ env.development.js
@@ -1,5 +1,5 @@
 module.exports = {
-  META_TITLE: "FC IMABARI Sailors' Club",
+  META_TITLE: "【開発】FC IMABARI Sailors'Club",
   ROBOTS: "noindex",
   BASE_URL: "https://dev-member-imabari.a.kuroco.app",
   PAYGENT_JS: "https://sandbox.paygent.co.jp/js/PaygentToken.js",
--- nuxt.config.js
+++ nuxt.config.js
@@ -30,9 +30,42 @@ export default {
       {
         hid: "description",
         name: "description",
-        content: process.env.npm_package_description || "",
+        content: envSettings.META_TITLE,
       },
       { hid: "robots", name: "robots", content: envSettings.ROBOTS },
+      {
+        hid: "og:description",
+        property: "og:description",
+        content: envSettings.META_TITLE,
+      },
+      {
+        hid: "og:title",
+        name: "og:title",
+        property: "og:title",
+        content: envSettings.META_TITLE,
+      },
+      {
+        hid: "og:site_name",
+        name: "og:site_name",
+        property: "og:site_name",
+        content: envSettings.META_TITLE,
+      },
+      {
+        hid: "og:type",
+        name: "og:type",
+        property: "og:type",
+        content: envSettings.META_TITLE,
+      },
+      {
+        hid: "apple-mobile-web-app-title",
+        name: "apple-mobile-web-app-title",
+        content: envSettings.META_TITLE,
+      },
+      {
+        hid: "mobile-web-app-capable",
+        name: "mobile-web-app-capable",
+        content: envSettings.META_TITLE,
+      },
     ],
     link: [
       { rel: "shortcut icon", type: "image/x-icon", href: "/favicon.ico" },
