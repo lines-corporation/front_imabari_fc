d234eac4dce89968779407da8e56a4ef996f0af3
yangyongji79 <yangyongji79@gmail.com>
2021-04-28 12:18:46 +0900

    購入履歴の修正

--- src/pages/ec/history/_id/index.vue
+++ src/pages/ec/history/_id/index.vue
@@ -69,32 +69,43 @@ export default {
       let self = this
       ids.forEach(async (id) => {
         const response = await self.$auth.ctx.$axios.get(`/rcms-api/1/shop/history/${id}`)
+        if(response.data.details.inst_ymdhi == undefined){
+          self.purchaseDate = "キャンセル済み"
+        } else {
         self.purchaseDate = response.data.details.inst_ymdhi.replace("+09:00", "").replace("T", " ").substring(0,16)
         self.note = response.data.details.note
         self.generic_payment_status = response.data.details.generic_payment_status.label
         self.totalPaymentPrice = parseInt(response.data.details.total)
-        response.data.details.order_details.forEach((order) => {
-          self.$auth.ctx.$axios.get(`/rcms-api/1/shop/product/${order.product_id}`).then((productInfoResponse) => {
-            if(productInfoResponse.data.details.product_data.ext_columns.straight[0] != undefined) {
-              self.products.push({
-                id:       order.product_id,
-                quantity: order.quantity,
-                title:    productInfoResponse.data.details.topics_name,
-                price:    response.data.details.payment_total,
-                size:     productInfoResponse.data.details.product_name,
-                image:    productInfoResponse.data.details.product_data.ext_columns.straight[0].file_url,
-              })
-            } else {
-              self.products.push({
-                id:       order.product_id,
-                quantity: order.quantity,
-                title:    productInfoResponse.data.details.topics_name,
-                price:    response.data.details.payment_total,
-                size:     productInfoResponse.data.details.product_name,
-              })
-            }
+          response.data.details.order_details.forEach((order) => {
+            self.$auth.ctx.$axios.get(`/rcms-api/1/shop/product/${order.product_id}`).then((productInfoResponse) => {
+              if(productInfoResponse.data.details.product_data.ext_columns.straight[0] != undefined) {
+                self.products.push({
+                  id:       order.product_id,
+                  quantity: order.quantity,
+                  title:    productInfoResponse.data.details.topics_name,
+                  price:    response.data.details.payment_total,
+                  size:     productInfoResponse.data.details.product_name,
+                  image:    productInfoResponse.data.details.product_data.ext_columns.straight[0].file_url,
+                })
+              } else {
+                self.products.push({
+                  id:       order.product_id,
+                  quantity: order.quantity,
+                  title:    productInfoResponse.data.details.topics_name,
+                  price:    response.data.details.payment_total,
+                  size:     productInfoResponse.data.details.product_name,
+                })
+              }
+            }).catch(function (error) {
+              self.$store.dispatch(
+                "snackbar/setError",
+                error.response.data.errors?.[0]
+              )
+              self.$store.dispatch("snackbar/snackOn")
+              self.loading2 = false
+            })
           })
-        })
+        }
       })
     }
   },
