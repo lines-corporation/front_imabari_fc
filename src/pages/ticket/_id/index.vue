1578b8c7eb47271daf3f08a31b9f16d213dc18d2
yangyongji79 <yangyongji79@gmail.com>
2021-05-06 18:40:07 +0900

    グループ別 割引設定をチケット価格表示に反映させる

--- src/pages/ticket/_id/index.vue
+++ src/pages/ticket/_id/index.vue
@@ -489,8 +489,10 @@
                             <tr>
                               <td>{{ t.subject }}</td>
                               <td>
+                                <br/>
                                 <span>{{ t.price_02 }}円</span> <br/>
-                                <span v-if="flag && t.group_price > 0"> 有料会員限定の割引価格 {{ t.group_price }}円</span>
+                                <span v-if="t.group_price > 0"> 有料会員限定の割引価格 {{ t.price_by_group.split(",")[0].split(":")[1] }}円</span><br/>
+                                <span v-if="t.group_price > 0"> 無料会員限定の割引価格 {{ t.price_by_group.split(",")[1].split(":")[1].split("}")[0] }}円</span>
                               </td>
                               <td>
                                 <v-select
@@ -717,7 +719,6 @@ export default {
     order_detail_id: 1,
     qrcode_type: 1,
     keyword: 0,
-    flag: false,
     item: [],
     seat_num: 0,
     product_list: [],
@@ -856,11 +857,6 @@ export default {
                 }
               }
             }
-          }
-          if(Object.values(JSON.parse(JSON.stringify(self.$auth.user.group_ids)))[0] == "無料会員"){
-            self.flag = false
-          } else {
-            self.flag = true
           }
           if((time - paymentTime > 0 && self.topics_id == "1036") || time - paymentTime <= 0){
             self.timeFlag = true
