5aeede4aa978e75b27a1a3b33d465a0f25858a83
yangyongji79 <yangyongji79@gmail.com>
2021-06-18 15:34:37 +0900

    カートボタン配置

--- src/pages/ec/_id/index.vue
+++ src/pages/ec/_id/index.vue
@@ -527,6 +527,7 @@ export default {
           return
         }
       })
+      window.location.reload()
     },
     async getCartItems() {
       if(!this.$auth.user || !this.$auth.user.ec_cart_id) {
